b3679a508b55ad33cbe02db67b5a9d2ee0ca5dc4
Per-Kristian <per_kb@hotmail.com>
2016-12-14 21:05:54 +0100

    Advent of code 1. Desember, not correct yet

---
+++ AdventOfCode_1.cpp
@@ -0,0 +1,115 @@
+#include <iostream>
+#include <fstream>
+using namespace std;
+
+class Grid {
+private:
+	int x, y;
+	char face;
+public:
+	Grid() { x = 0; y = 0; face = 'N'; }
+	void directionChange(char d);
+	void posisionChange(int n);
+	void skrivAlt();
+	//void lesFraFil(ifstream & inn, ch);
+};
+
+void lesFraFil();
+
+Grid Bunny;
+
+int main() {
+	lesFraFil();
+	Bunny.skrivAlt();
+	return 0;
+}
+
+void Grid::directionChange(char d) {
+	if (face == 'N' && d == 'R')
+	{
+		face = 'E';
+	}
+	else if (face == 'N' && d == 'L')
+	{
+		face = 'W';
+	}
+	else if (face == 'E' && d == 'R')
+	{
+		face = 'S';
+	}
+	else if (face == 'E' && d == 'L')
+	{
+		face = 'N';
+	}
+	else if (face == 'S' && d == 'R')
+	{
+		face = 'W';
+	}
+	else if (face == 'S' && d == 'L')
+	{
+		face = 'E';
+	}
+	else if (face == 'W' && d == 'R')
+	{
+		face = 'N';
+	}
+	else if (face == 'W' && d == 'L')
+	{
+		face = 'S';
+	}
+	else
+	{
+		cout << "Direction Error";
+	}
+}
+
+void Grid::posisionChange(int n) {
+	switch (face)
+	{
+	case 'N': y += n; break;
+	case 'E': x += n; break;
+	case 'S': y -= n; break;
+	case 'W': x -= n; break;
+	default: cout << "Posision Error";
+		break;
+	}
+}
+
+void Grid::skrivAlt() {
+	cout << "X: " << x << '\n' << "Y: " << y << '\n';
+	cout << "Antall blocks away: " << x + y << endl;
+}
+
+//void Grid::lesFraFil(ifstream & inn, ch) {
+
+
+void lesFraFil() {
+	ifstream innfil("AdventOfCode_1.txt");
+	char ch;
+	if (innfil)
+	{
+		innfil >> ch;
+		while (!innfil.eof())
+		{
+			if (isdigit(ch))
+			{
+				int i = ch - '0';
+				Bunny.posisionChange(i);
+			}
+			else if (isalpha(ch))
+			{
+				Bunny.directionChange(ch);
+			}
+			else
+			{
+				cout << "Read Error";
+			}
+			innfil >> ch;
+		}
+
+	}
+	else
+	{
+		cout << "Fil ikke funnet!" << endl;
+	}
+}
