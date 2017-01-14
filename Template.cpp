eddf6547835209b064d19b3b50d435965ea12e9c
Per-Kristian <per_kb@hotmail.com>
2017-01-14 21:33:20 +0100

    Added cpp template file

---
+++ Template.cpp
@@ -0,0 +1,41 @@
+// 
+//
+//
+//
+//
+
+
+//  INCLUDE:
+
+
+//  CONST:
+
+//  ENUM:
+
+//  STRUCT
+
+//  KLASSE:
+class  {
+private:
+
+public:
+
+};
+
+//  DEKLARASJON AV FUNKSJONER:
+
+
+//  GLOBALE VARIABLE:
+
+
+int main() {           //  HOVEDPROGRAM:
+
+	return 0;
+}
+
+//  DEFINISJON AV KLASSE-FUNKSJONER:
+
+
+
+//  DEFINISJON AV FUNKSJONER:
+
