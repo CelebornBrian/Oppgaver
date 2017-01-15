9cc507f7923b5452089bec13db149363446a9701
Per-Kristian <per_kb@hotmail.com>
2017-01-15 23:20:04 +0100

    Unfinished version of Oppgave 22

--- Oppgave22.cpp
+++ Oppgave22.cpp
@@ -25,20 +25,24 @@ const int MAXSTILKAR = 20;
 class Hopper {
 private:
 	char navn[STRLEN];
-	int poengsum,
-		lengde;
+	float poengsum;
+	int lengde;
 	int stilKarakter[ANTALLSTILKARAKTERER];
+	void sorterKarakter();
 public:
 	Hopper();
 	void les2(char n[]);
 	void hopp();
+	void poengSum();
+	void skrivNavn();
 };
 
 //  DEKLARASJON AV FUNKSJONER:
 
-void les(const char t[], char s[], const int LEN);
-//int les(const char t[], const int min, const int max);
-float les(const char t[], const int min, const int max);//Les tall
+void les(const char t[], char s[], const int LEN);		//Les tekst
+int les(const char t[], const int min, const int max);//Les tall
+void regnPoengsum();
+void skrivAlle();
 
 //  GLOBALE VARIABLE:
 
@@ -50,9 +54,16 @@ int main() {
 	char navn[STRLEN];
 	for (int i = 1; i <= ANTALLHOPPERE; i++)
 	{
-		les("Navn: ", navn, STRLEN);
+		les("Navn paa deltaker", navn, STRLEN);
 		hoppere[i].les2(navn);
 	}
+	for (int i = 1; i <= ANTALLHOPPERE; i++)
+	{
+		cout << "Statistikk for hoppet til "; 
+		hoppere[i].skrivNavn(); cout << ": ";
+		hoppere[i].hopp();
+	}
+	skrivAlle();
 	return 0;
 }
 
@@ -66,27 +77,75 @@ void Hopper::les2(char n[]) {
 	strcpy(navn, n);
 }
 void Hopper::hopp() {
-	lengde = ::les("Lengde på hopp: ", MINHOPPLENGDE, MAXHOPPLENGDE);
+	lengde = les("Lengde på hopp", MINHOPPLENGDE, MAXHOPPLENGDE);
 	for (int i = 0; i < ANTALLSTILKARAKTERER; i++)
 	{
-		stilKarakter[i] = ::les("Stilkarakter: ", MINSTILKAR, MAXSTILKAR);
+		stilKarakter[i] = les("Stilkarakter", MINSTILKAR, MAXSTILKAR);
+	}
+}
+
+void Hopper::poengSum()
+{
+	float lengdePoeng, stilPoeng;
+	sorterKarakter();
+	lengdePoeng = (70 + ((lengde - 80) * 1.4));
+	stilPoeng = (stilKarakter[1] + stilKarakter[2] + stilKarakter[3]);
+	poengsum = (lengdePoeng + stilPoeng);
+}
+void Hopper::sorterKarakter() {
+	int i, j, temp = stilKarakter[0];
+	for (i = 0; i < ANTALLSTILKARAKTERER-1; i++)
+	{
+		for (j = i + 1; j < ANTALLSTILKARAKTERER; j++)
+		{
+			if (stilKarakter[i] < stilKarakter[j])
+			{
+				temp = stilKarakter[i];
+				stilKarakter[i] = stilKarakter[j];
+				stilKarakter[j] = temp;
+			}
+		}
+	}
+}
+void Hopper::skrivNavn() {
+	if (poengsum)
+	{
+		cout << navn << " med " << poengsum << " antall poeng." << endl;
+	}
+	else
+	{
+		cout << navn;
 	}
 }
 
 //  DEFINISJON AV FUNKSJONER:
 
 //  Leser et tall i et visst intervall:
-float les(const char t[], const int min, const int max) {
-	float n;
+int les(const char t[], const int min, const int max) {
+	int n;
 	do {                               // Skriver ledetekst:
 		cout << '\t' << t << " (" << min << '-' << max << "): ";
 		cin >> n;   cin.ignore();         // Leser inn ett tall.
 	} while (n < min || n > max);      // Sjekker at i lovlig intervall.
 	return n;                           // Returnerer innlest tall.
 }
+void regnPoengsum()
+{
+	for (int i = 1; i <= ANTALLHOPPERE; i++)
+	{
+		hoppere[i].poengSum();
+	}
+}
 //  Leser en ikke-blank tekst:
 void les(const char t[], char s[], const int LEN) {
 	do {
-		cout << '\t' << t << ": ";	cin.getline(s, LEN); //  Ledetekst og leser.
+		cout << t << ": ";	cin.getline(s, LEN); //  Ledetekst og leser.
 	} while (strlen(s) == 0);           //  Sjekker at tekstlengden er ulik 0.
+}
+
+void skrivAlle() {
+	for (int i = 1; i <= ANTALLHOPPERE; i++)
+	{
+		hoppere[i].skrivNavn();
+	}
 }
