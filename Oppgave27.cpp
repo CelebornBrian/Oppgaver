83eea1a56ac804444c017bd1fa346948f76924ad
Per-Kristian <per_kb@hotmail.com>
2017-01-19 14:11:27 +0100

    Oppgave 27 done

--- Oppgave27.cpp
+++ Oppgave27.cpp
@@ -7,56 +7,118 @@ class Tid {
 private:
 	int time;
 	int minutt;
-
+	
 public:
 	Tid() { time = 0; minutt = 0; }
 	Tid(int t, int m) { time = t; minutt = m; }
+	void skrivTid();
 
-	Tid& operator+=(int rhs);
+	void operator += (const int rhs);
+	void operator -= (const int rhs);
+	Tid  operator + (const Tid & rhs);
+	Tid operator - (const Tid & rhs);
+	bool operator == (const Tid & rhs);
+	bool operator < (const Tid & rhs);
 
 };
 
-void skrivMeny();
-char les();
+
 
 int main() {
-	char kommando;
-	Tid t1(9, 30), t2(19, 32), t3;
-	skrivMeny();
-	cout << "\nCommand: "; kommando = les();
-	while (kommando != 'Q') {
-		switch (kommando) {
-		case 'A': t1 += 47;			break;	//  Oppgave 2C  
-											//case 'U': nyttUtlegg();			break;	//  Oppgave 2D
-											//case 'B': registrerBetaling();	break;	//  Oppgave 2E
-											//case 'D': deltagerneSkylder();	break;	//  Oppgave 2F
-		default:  skrivMeny();			break;
-		}
-		cout << "\nØnske:  ";  kommando = les();
-	}
-	cout << "\n\n";
+	Tid t1(15, 29), t2(15, 30), t3;
+	t1.skrivTid(); t1 += 47; t1.skrivTid();
+
+	t2.skrivTid(); t2 -= 16; t2.skrivTid();
+
+	t3 = t1 + t2; t3.skrivTid();
+
+	t3 = t1 - t2; t3.skrivTid();
+
+	cout << ((t1 == t2) ? "Like" : "Ulike") << endl;
+
+	cout << ((t1 < t2) ? "Mindre" : "Ikke Mindre") << endl;
+
 	return 0;
 }
 
-// **********************   DEFINISJON AV FUNKSJONER:    *********************
-
-void skrivMeny() {          //  Presenterer lovlige menyvalg:
-	cout << "\n\nFØLGENDE KOMMANDOER ER LOVLIG:\n";
-	cout << "\tA = Legges til antall minutter i objektet \n";
-	//cout << "\tU = nytt Utlegg har forekommet\n";
-	//cout << "\tB = Betaling foretatt av en av deltagerne\n";
-	//cout << "\tD = hva hver av Deltagerne skylder\n";
-	cout << "\tQ = Quit/avslutt\n";
-}
+// **********************   DEFINISJON AV KLASSEFUNKSJONER:    *********************
 
 
-char les() {			//  Henter ett ikke-blankt upcaset tegn:
-	char ch;
-	cin >> ch;   cin.ignore();    //  Leser ETT tegn. Forkaster '\n'.
-	return (toupper(ch));         //  Upcaser og returnerer.
-}
-
-Tid & Tid::operator+=(int rhs)
+void Tid::operator += (const int rhs)
 {
-	// TODO: insert return statement here
+	minutt += rhs;
+	while (minutt > 59)
+	{
+		time++;
+		minutt -= 60;
+	}
+}
+
+void Tid::operator -= (const int rhs) {
+	minutt -= rhs;
+	while (minutt < 0)
+	{
+		time--;
+		minutt += 60;
+	}
+}
+
+Tid Tid::operator + (const Tid & rhs) {
+	Tid temp;
+	temp.minutt = minutt + rhs.minutt;
+	while (temp.minutt > 59)
+	{
+		temp.time++;
+		temp.minutt -= 60;
+	}
+	temp.time += time + rhs.time;
+	return temp;
+}
+
+Tid Tid::operator - (const Tid & rhs) {
+	Tid temp;
+	temp.minutt = (minutt - rhs.minutt);
+	while (temp.minutt < 0)
+	{
+		temp.time--;
+		temp.minutt += 60;
+	}
+	temp.time -= (time - rhs.time);
+	return temp;
+}
+
+bool Tid::operator == (const Tid & rhs) {
+	if (minutt != rhs.minutt)
+	{
+		return false;
+	}
+	else if (time != rhs.time)
+	{
+		return false;
+	}
+	else return true;
+}
+
+bool Tid::operator < (const Tid & rhs) {
+	if (time < rhs.time)
+	{
+		return true;
+	}
+	else if (time == rhs.time && minutt < rhs.minutt)
+	{
+		return true;
+	}
+	else return false;
+}
+
+void Tid::skrivTid() {
+	if (time > 0)
+	{
+	cout << ((time < 10) ? "0" : "") << time << ":"
+		<< ((minutt < 10) ? "0" : "") << minutt << endl;
+	}
+	else
+	{
+		cout << "Ugyldig Tid!" << endl;
+	}
 }
